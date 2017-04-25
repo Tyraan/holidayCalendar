7e99657f59b68916c6217a42fa6e15296d9a3ba4
Tyraan T <tomkly000@gmail.com>
2017-04-25 18:34:00 +0800

    fix the chart

--- makePic.py
+++
@@ -1,8 +0,0 @@
-import os
-if os.path.exists('dota2.db'):
-    import numpy as np
-    import matplotlib.pyplot as plt
-    import sqlite3
-    conn= sqlite3.connect('dota2.db')
-    cur = conn.cursor()
-    cur
